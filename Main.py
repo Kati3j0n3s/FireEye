b58df4ca67153e2951651fe2596cf05e5568aa35
KatlynK1500 <144631018+KatlynK1500@users.noreply.github.com>
2024-11-20 11:12:51 -0800

    Reviewing The Code
    From the knowledge gained from Lab 8, I'm going back and modifying the code.

--- Main.py
+++ Main.py
@@ -1,5 +1,12 @@
+'''
+Before I go any further, I'm going to create some sort of diagram to help me out in coding. 
+I already feel that I need one.
+'''
+
+
 # Importing Libraries
 import RPi.GPIO as GPIO
+import PCF8591 as ADC   # Not needed yet, but I think I do....
 import time
 
 # Referencing the other py files
@@ -7,13 +14,19 @@ from StartUpSequence import *
 from Diagnostic import *
 from ButtonHandler import *
 
-# Establishing sensor pins via physical pin numbering
-GPIO.setmode(GPIO.BOARD)
+# Configures GPIO to use Broadcom chip numbering scheme.
+GPIO.setmode(GPIO.BCM)
 BtnPin = 12
 TempPin = 11
 HumidityPin = 13
 
+# MAKE SURE TO ADD THIS INTO THE MAIN LOOP
+def setup():
+  GPIO.setup(BtnPin, GPIO.IN, pull_up_down = GPIO.PUD_UP) # Probably need to change PUD_UP
+  GPIO.setup(TempPin, GPIO.IN)
+  GPIO.setup(HumidityPin, GPIO.IN)
 
+# Need to rename this, just have the 'main' loop be the python one
 def main():
   # Try to start up, if it fails enters a loop to re-run diagnostics
   start_up_status = start_up_sequence(BtnPin, TempPin, HumidityPin)
