73033307eb7e8d930572d5ad54b621eee16cecfa
KatlynK1500 <144631018+KatlynK1500@users.noreply.github.com>
2024-11-20 13:02:38 -0800

    Update StartUpSequence.py

--- StartUpSequence.py
+++ StartUpSequence.py
@@ -1,19 +1,29 @@
-# Start up sequence to run every time it turns on.
-# Checking for sensor communication.
+'''
+StartUpSequenc.py houses the start up logic. It either ends with the
+Rasbperry pi waiting for altitude change, or with Fail and will not
+allow flight.
 
+Checks for:
+- Connection to Sensors
+- Enough battery life, which is a function call
+
+
+NOTE
+This logic is placed in Main.py for now and will be moved here.
+'''
 
 
 # Referencing other py files
 from Diagnostic import *
 
-def start_up_sequence(BtnPin, TempPin, HumidityPin):
-    print("Starging up...")
+# def start_up_sequence(BtnPin, TempPin, HumidityPin):
+#     print("Starging up...")
 
-    diagnostic_passed = diagnostic_check(BtnPin, TempPin, HumidityPin)
+#     diagnostic_passed = diagnostic_check(BtnPin, TempPin, HumidityPin)
 
-    if not diagnostic_passed:
-        print("Startup failed: Initial diagnostic check did not pass.")
-        return False
+#     if not diagnostic_passed:
+#         print("Startup failed: Initial diagnostic check did not pass.")
+#         return False
 
-    print("Startup successful.")
-    return True
+#     print("Startup successful.")
+#     return True
